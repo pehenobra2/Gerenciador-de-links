f7d0be781a452322cfb541e29a1520f60d263fcd
pehenobra2 <peheguibra@gmail.com>
2024-04-03 12:34:28 -0300

    refazendo a tela de login

--- site/src/app/app.routes.ts
+++ site/src/app/app.routes.ts
@@ -1,17 +1,9 @@
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-  {path: "login", component: LoginComponent},
-  {path: "", redirectTo:"login", pathMatch:"full"},
-  {path: "register", component: RegisterComponent}
+  { path: "login", component:LoginComponent}
 ];
 
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
